68bbd938a26ded56983df7bc3df6749ddda94652
lovac42 <lovac42@protonmail.com>
2020-01-23 07:10:25 -0500

    changed order to test travis stuff first

--- night_mode/__init__.py
+++ night_mode/__init__.py
@@ -1,26 +1,28 @@
-from aqt import mw
-from anki.hooks import addHook, remHook
-
 
 #addons should selectively load before or after a delay of 666
 NM_RESERVED_DELAY = 666
 
 
-def delayedLoader():
-    from .night_mode import NightMode
-    night_mode = NightMode()
-    night_mode.load() #Manual loading first profile
-
-def onProfileLoaded():
-    remHook("profileLoaded", onProfileLoaded)
-    mw.progress.timer(
-        NM_RESERVED_DELAY, delayedLoader, False
-    )
-
-
 try: #travis
+
     from anki_testing import anki_running
     from .night_mode import NightMode
     night_mode = NightMode()
-except:
+
+except ModuleNotFoundError:
+
+    from aqt import mw
+    from anki.hooks import addHook, remHook
+
+    def delayedLoader():
+        from .night_mode import NightMode
+        night_mode = NightMode()
+        night_mode.load() #Manual loading first profile
+
+    def onProfileLoaded():
+        remHook("profileLoaded", onProfileLoaded)
+        mw.progress.timer(
+            NM_RESERVED_DELAY, delayedLoader, False
+        )
+
     addHook('profileLoaded', onProfileLoaded)
